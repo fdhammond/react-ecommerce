35d0085ca0175bc61991501f0bd3499f4ba59beb
fd <federico.hammond@gmail.com>
2021-08-30 21:14:56 -0300

    add navbar

---
+++ src/components/Nav/NavBar.jsx
@@ -0,0 +1,47 @@
+import React, {Fragment} from 'react';
+
+import '././../../App.css'
+import Navbar from 'react-bootstrap/NavBar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
+import { BsBag } from "react-icons/bs";
+
+const NavBar = () => {
+    return (
+        <Fragment>
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">MusicLand</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#features"><a className='nav-warning--color'>Features</a></Nav.Link>
+      <Nav.Link href="#pricing"><a className='nav-warning--color'>Pricing</a></Nav.Link>
+    </Nav>
+    <Nav>
+      <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-3"
+        aria-label="Search"
+      />
+      <Button variant="outline-warning" className="nav-spacing">Search</Button>
+    </Form>
+    <Nav.Link className="nav-spacing">
+      <BsBag className="nav-warning--color"/>
+    </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+  </Navbar>
+  </Fragment>
+    );
+};
+
+export default NavBar;
