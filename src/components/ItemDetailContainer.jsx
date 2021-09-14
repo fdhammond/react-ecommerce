bed4f8934506d14d7da0349806c70d7993541224
fd <federico.hammond@gmail.com>
2021-09-14 00:00:41 -0300

    working on itemDetail view

---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ItemDetailContainer = () => {
+    return (
+        <Container>
+            <Container>
+                <Row className="mt-3">
+                <h1>{ greeting }</h1>
+                </Row>
+            </Container>
+            <Container>
+            <Row>
+                <Col className="mt-3"><ItemDetail /></Col>
+            </Row>
+            </Container>
+        </Container>
+    );
+};
+
+export default ItemDetailContainer;
