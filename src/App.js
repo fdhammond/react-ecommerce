569fae25807cedb8d908a100425616740f525e52
fede <federico.hammond8@gmail.com>
2021-09-14 21:18:18 -0300

    add first routing for components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Nav/NavBar';
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
 import ItemListContainer from './components/ItemListContainer';
 import Footer from './components/Footer';
+import Cart from './components/Cart';
 
 function App() {
   return (
+    <BrowserRouter>
     <>
-      <NavBar />
-      <ItemListContainer
-        greeting="Hello from ItemListContainer"
-      />
+          <NavBar />
+        <Switch>
+            <Route exact path='/'>
+          <ItemListContainer
+          greeting="Hello from ItemListContainer"
+          />
+            </Route>
+            <Route exact path='/cart' component={Cart} />
       <Footer/>
+        </Switch>
     </>
+      </BrowserRouter>
   );
 }
 
